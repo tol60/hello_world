1fa0dae77340f2a2bb1f8fb7372dbca78fac339c
Tony <tol60@hotmail.com>
2025-07-10 22:02:19 -0400

    ch2_bug_examples

---
+++ CH2_COMPILE_BUG/HelloStraus_Bug1.C
@@ -0,0 +1,9 @@
+// Just a simple hello world test
+// g++ -std=c++11 -g -o HelloStraus_Bug1.exe HelloStraus_Bug1.C  2>&1 | tee tmp.txt
+
+int main ()
+{
+  cout << "Hello, Straus!\n";
+  //keep_window_open(); // wait for a character to be entered
+  return 0;
+}
---
+++ CH2_COMPILE_BUG/HelloStraus_Bug5.C
@@ -0,0 +1,10 @@
+// Just a simple hello world test
+// g++ -std=c++11 -g -o HelloStraus_Bug5.exe HelloStraus_Bug5.C  2>&1 | tee tmp.txt
+
+#include "std_lib_facilities.h"
+int main ()
+{
+  cout < "Hello, Straus!\n";
+  //keep_window_open(); // wait for a character to be entered
+  return 0;
+}
